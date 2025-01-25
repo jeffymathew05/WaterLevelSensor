069b356d568abf0b0ec06b0ee7d9c8333f500537
jeffymathew05 <157703661+jeffymathew05@users.noreply.github.com>
2025-01-25 23:54:13 +0530

    Add files via upload

--- Water level Sensor/TransmitterSide/TransmitterSide.ino
+++ Water level Sensor/TransmitterSide/TransmitterSide.ino
@@ -1,32 +1,44 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-#define FLOAT_SENSOR1  1     // the number of the float switch1 pin
-#define FLOAT_SENSOR2  2     // the number of the float switch2 pin
+#define ss 15
+#define rst 16
+#define dio0 2
+#define FLOAT_SENSOR1  D2                       // the number of the float switch1 pin
+#define FLOAT_SENSOR2  D1                       // the number of the float switch2 pin
+
+int sensor1,sensor2;
+char counter = '0';
 void setup() {
 
   Serial.begin(9600);
-  while (!Serial);      // Waits for the serial port to be ready
-
-  if (!LoRa.begin(915E6)) {      // Initializes the LoRa module with a frequency of 915 MHz
-    Serial.println("Starting LoRa failed!");      // If fails to initialize the LoRa module, the message "Starting LoRa failed!" is printed to the Serial Monitor
-    while (1);      // the program halts to an infinite loop if the initializing is failed
+  while (!Serial);                               // Waits for the serial port to be ready
+  LoRa.setPins(ss, rst, dio0);
+  if (!LoRa.begin(915E6)) {                      // Initializes the LoRa module with a frequency of 915 MHz
+    Serial.println("Starting LoRa failed!");     // If fails to initialize the LoRa module, the message "Starting LoRa failed!" is printed to the Serial Monitor
+    while (1);                                   // the program halts to an infinite loop if the initializing is failed
   }
 
-  pinMode(FLOAT_SENSOR1, INPUT_PULLUP);     // initialize the first float switch pin as an input:
-  pinMode(FLOAT_SENSOR2, INPUT_PULLUP);     // initialize the second float switch2 pin as an input:
+  pinMode(FLOAT_SENSOR1, OUTPUT);                 // initialize the first float switch pin as an input:
+  pinMode(FLOAT_SENSOR2, OUTPUT);                 // initialize the second float switch2 pin as an input:
+  
 }
 
-void loop() {
-
-  int sensor1=digitalRead(FLOAT_SENSOR1);     //taking value from first float switch:
-  int sensor2=digitalRead(FLOAT_SENSOR2);     //taking value from second float switch:
+void loop() { 
+  digitalWrite(FLOAT_SENSOR1,HIGH);                //Taking Value from first sensor, making it high is for testing the module
+  digitalWrite(FLOAT_SENSOR2,HIGH);                //Taking Value from second sensor, making it high is for testing the module
+  sensor1=digitalRead(FLOAT_SENSOR1);              //taking value from first float switch:
+  sensor2=digitalRead(FLOAT_SENSOR2);              //taking value from second float switch:
+  if(sensor1==0&&sensor2==0){
+    counter='1';
+  }
+  else if(sensor1==1&&sensor2==1){
+    counter='0';
+  }
   Serial.print("Sending packet: ");
-  LoRa.beginPacket();                         //started the packet to send values to receiver side:
-  LoRa.print(sensor1);                        //the value from the first float sensor is stored in the packet:
-  LoRa.print(sensor2);                        //the value from the second float sensor is stored in the packet:
-  LoRa.endPacket();                           //ends the packet to send values to receiver side:
-  
-  delay(5000);
-  
+  Serial.println(counter);
+  LoRa.beginPacket();
+  LoRa.println(counter);                       
+  LoRa.endPacket();                           
+  delay(3000); 
 }
